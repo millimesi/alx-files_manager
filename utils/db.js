78f0f49f8fba4d45cc4632a920b5ddc2e7040a72
millimesi <millionmesi1@gmail.com>
2024-08-05 12:46:31 +0300

    Task 0 is added

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,26 @@
-
+import { mongoClient } from 'mongodb';
 
 class DBClient {
+    /**
+     * Create host, port and database from enviroment variable
+     * or takes a default value
+     */
     constructor() {
-        
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const dburl = `mongodb://${host}:${port}/${database}`;
+
+        // create mongo client and connect 
+        this.client = new mongoClient(dburl, { useUnifiedTopology: true });
+        this.client.connect();
+    }
+
+    /**
+     * isAlive checks if the client is connected with the database server
+     * @returns: boolean if connected true else false
+     */
+    isAlive() {
+        this.client.isConnected();
     }
 }
